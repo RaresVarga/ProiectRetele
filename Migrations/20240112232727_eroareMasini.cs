ea79f2cc10ebb9af6ac3a9fa58e4ebd09fe11f56
SEBASTIAN-RAREȘ VARGA <sebastian.varga@stud.ubbcluj.ro>
2024-01-13 01:18:12 +0200

    Dupa creere relatii intre toate tabelele

---
+++ Migrations/20240112232727_eroareMasini.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProiectRetele.Migrations
+{
+    public partial class eroareMasini : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientID",
+                table: "Masina",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Masina_ClientID",
+                table: "Masina",
+                column: "ClientID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Masina_Client_ClientID",
+                table: "Masina",
+                column: "ClientID",
+                principalTable: "Client",
+                principalColumn: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Masina_Client_ClientID",
+                table: "Masina");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Masina_ClientID",
+                table: "Masina");
+
+            migrationBuilder.DropColumn(
+                name: "ClientID",
+                table: "Masina");
+        }
+    }
+}
